5afab6f4a70e168e2a65310a586ca60d7e6b90e4
MutaiAllan <MutaiAllan>
2023-06-28 18:25:20 +0300

    Added solutions to the lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reverseWord = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  if (word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
+//console.log(isPalindrome("cook"))
+//console.log(isPalindrome("level"))
 
 /* 
   Add your pseudocode here
@@ -8,6 +18,10 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  1.Create a variable which will store the word in a reverse form.
+    Using for loop, append each letter of the word to the new variable from the last letter of the word to the first one.
+  2.compare the new variable with the word being passed as argument.
+    If the words match, return true, else return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +34,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("cat"));
+
+  console.log("");
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("javascript"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
